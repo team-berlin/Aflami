385b8e2c34fbf3f811d744e87e90944e52a9a09a
Eman Abobakr <eman.abobakr@vodafone.com>
2025-06-29 23:31:12 +0300

    change applicationId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.aflami"
+        applicationId = "com.berlin.aflami"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
