59036bbdd196a00209073bb0eaf9acc4aa862bb5
mohamedshemees <mohamedessampd@gmail.com>
2025-06-30 02:02:25 +0300

    Add Kover plugin for test coverage and configure build checks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kover)
 }
 
 android {
@@ -56,4 +57,24 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+
+kover {
+    reports {
+        filters {
+            includes {
+                //modules to be added
+                //projects(module1,module2,module3)
+
+            }
+        }
+        verify {
+            rule {
+                bound {
+                    minValue = 80
+                }
+            }
+        }
+    }
 }
