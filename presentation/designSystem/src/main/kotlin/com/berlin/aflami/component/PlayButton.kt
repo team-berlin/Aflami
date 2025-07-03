fd05dc4c2a15217a481b6e033b9bae762d65011c
Ahmed Mahgoub <aed7862@gmail.com>
2025-07-03 13:17:21 +0300

    feat: add play button component

---
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/PlayButton.kt
@@ -0,0 +1,93 @@
+package com.berlin.aflami.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.berlin.aflami.modifier.dropShadow
+import com.berlin.aflami.ui.theme.AflamiTheme
+import com.berlin.aflami.ui.theme.Theme
+import com.berlin.designsystem.R
+
+@Composable
+fun PlayButton(
+    modifier: Modifier = Modifier,
+    hasDropShadow: Boolean = false,
+    backgroundColor: Color = Theme.color.surfaceHigh,
+    size: Int = 40,
+    borderWidth: Int = 1,
+    dropShadowColor: Color = Theme.color.primary,
+    dropShadowAlpha: Float = 0.03f,
+    blur: Int = 4,
+    onClickPlay: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .size(size.dp)
+            .then(
+                if (hasDropShadow)
+                    Modifier.dropShadow(
+                        shape = RoundedCornerShape(75),
+                        color = dropShadowColor,
+                        alpha = dropShadowAlpha,
+                        blur = blur.dp,
+                        offsetY = 4.dp
+                    )
+                else Modifier
+            )
+            .clip(RoundedCornerShape(50))
+            .clickable { onClickPlay() }
+            .background(backgroundColor)
+            .border(
+                width = borderWidth.dp,
+                color = Theme.color.stroke,
+                shape = RoundedCornerShape(50)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.play),
+            contentDescription = stringResource(R.string.play_button),
+            tint = Theme.color.primary,
+            modifier = Modifier.size((size / 2).dp)
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun PlayButtonPreview() {
+    AflamiTheme(isDarkTheme = false) {
+        PlayButton(
+            hasDropShadow = true,
+            size = 64,
+            borderWidth = 2,
+            onClickPlay = {}
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun DarkPlayButtonPreview() {
+    AflamiTheme(isDarkTheme = true) {
+        PlayButton(
+            hasDropShadow = true,
+            size = 64,
+            borderWidth = 2,
+            onClickPlay = {}
+        )
+    }
+}
