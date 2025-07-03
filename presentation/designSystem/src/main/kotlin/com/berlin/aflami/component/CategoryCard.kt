bd53fc32bc1464486a1e584f601e0ad1b9b1d8b8
nada <nadamansoursamir@gmail.com>
2025-07-03 16:12:09 +0300

    add Category Card component

--- presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/CategoryCard.kt
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/CategoryCard.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
@@ -71,7 +72,7 @@ fun Preview() {
     AflamiTheme {
         CategoryCard(
             modifier = Modifier,
-            "Western",
+            stringResource(R.string.action),
             R.drawable.action_img
         )
     }
