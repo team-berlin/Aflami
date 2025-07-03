f7976756f0fa10b7684220147820192989f966f3
norhanadel <norhanadel367@gmail.com>
2025-07-03 20:19:01 +0300

    resolve conflig whe reset in comment 96aca39

--- presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/MoodPicker.kt
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/MoodPicker.kt
@@ -30,13 +30,12 @@ import androidx.compose.ui.unit.dp
 import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
 import com.berlin.designsystem.R
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 
 @Composable
 fun MoodPicker(
     modifier: Modifier = Modifier,
+    selectedMood: Int? = null,
+    onMoodSelected: (Int) -> Unit = {},
     onGetNowClicked: () -> Unit = {}
 ) {
     Box(
@@ -70,6 +69,8 @@ fun MoodPicker(
         MoodPickerHeader()
         MoodPickerContent(
             modifier = Modifier.padding(top = 76.dp, start = 2.dp, end = 2.dp, bottom = 2.dp),
+            selectedMood = selectedMood,
+            onMoodSelected = onMoodSelected,
             onGetNowClick = onGetNowClicked
         )
     }
@@ -95,9 +96,10 @@ private fun MoodPickerHeader(
 @Composable
 private fun MoodPickerContent(
     modifier: Modifier = Modifier,
+    selectedMood: Int? = null,
+    onMoodSelected: (Int) -> Unit,
     onGetNowClick: () -> Unit
 ) {
-
     val moodIcons = remember {
         listOf(
             R.drawable.ic_sad,
@@ -128,30 +130,29 @@ private fun MoodPickerContent(
         ) {
             moodIcons.forEach { iconRes ->
                 MoodIcon(
+                    modifier = Modifier,
                     iconRes = iconRes,
-                    isSelected = selectedIcon == iconRes,
-                    onClick = {
-                        selectedIcon = if (selectedIcon == iconRes) null else iconRes
-                    }
+                    isSelected = selectedMood == iconRes,
+                    onClick = { onMoodSelected(iconRes) }
                 )
             }
         }
         Text(
             text = stringResource(R.string.mood_picker_get_now),
             style = Theme.textStyle.body.medium,
-            color = if (selectedIcon != null) Theme.color.primary else Theme.color.disable,
+            color = if (selectedMood != null) Theme.color.primary else Theme.color.disable,
             modifier = Modifier
                 .padding(top = 12.dp, bottom = 4.dp)
-                .clickable(enabled = selectedIcon != null, onClick = onGetNowClick)
+                .clickable(enabled = selectedMood != null, onClick = onGetNowClick)
         )
     }
 }
 
 @Composable
 private fun MoodIcon(
-    modifier: Modifier = Modifier,
+    modifier: Modifier,
     iconRes: Int,
-    isSelected: Boolean,
+    isSelected: Boolean = false,
     onClick: () -> Unit
 ) {
     Icon(
@@ -160,11 +161,11 @@ private fun MoodIcon(
         tint = if (isSelected) Theme.color.primary else Theme.color.textColors.body,
         modifier = modifier
             .padding(4.dp)
+            .clip(CircleShape)
             .size(24.dp)
             .clickable(onClick = onClick)
     )
 }
-
 
 @Composable
 private fun BlurredIcon(
