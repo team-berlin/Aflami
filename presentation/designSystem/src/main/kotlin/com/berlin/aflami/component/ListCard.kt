f91f87b222e2c5caf6499b1c4819d75c2bdd4b43
NadeenRR <nadeenradwan0@gmail.com>
2025-07-03 19:54:04 +0300

    - feat: add list card component

---
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/ListCard.kt
@@ -0,0 +1,89 @@
+package com.berlin.aflami.component
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.berlin.aflami.ui.theme.AflamiTheme
+import com.berlin.aflami.ui.theme.Theme
+
+
+@Composable
+fun ListCard(
+    title: String,
+    itemCount: String
+) {
+    AflamiTheme(isDarkTheme = false) {
+        Box(
+            modifier = Modifier
+                .size(width = 160.dp, height = 147.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(width = 160.dp, height = 135.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            bottomEnd = 24.dp,
+                            bottomStart = 24.dp,
+                            topEnd = 16.dp,
+                            topStart = 16.dp
+                        )
+                    )
+                    .background(Theme.color.surfaceHigh)
+                    .align(Alignment.BottomEnd)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .align(Alignment.CenterStart)
+                        .offset(y = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        title,
+                        color = Theme.color.textColors.title,
+                        style = Theme.textStyle.title.medium
+                    )
+                    Text(
+                        itemCount,
+                        color = Theme.color.textColors.hint,
+                        style = Theme.textStyle.label.large
+                    )
+                }
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(width = 80.dp, height = 33.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topEnd = 16.dp,
+                            topStart = 24.dp
+                        )
+                    )
+                    .background(Theme.color.surfaceHigh)
+            )
+        }
+    }
+}
+
+@ThemeAndLocalePreviews
+@Composable
+private fun ListCardPreview() {
+    ListCard(
+        title = "My favorite",
+        itemCount = "12 item"
+    )
+}
