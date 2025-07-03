27877a332184fe8564fccb8739eaa45b0eed0adc
nada <nadamansoursamir@gmail.com>
2025-07-03 14:21:21 +0300

    add CategoryCard component

---
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/CategoryCard.kt
@@ -0,0 +1,89 @@
+package com.berlin.aflami.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.berlin.aflami.ui.theme.AflamiTheme
+import com.berlin.aflami.ui.theme.Theme
+import com.berlin.designsystem.R
+
+
+@Composable
+fun CategoryCard(
+    modifier: Modifier = Modifier,
+    mediaType: String,
+    mediaImgId: Int
+) {
+
+    val strokColor = Theme.color.stroke
+    Row(
+        modifier
+            .background(Theme.color.surfaceHigh, RoundedCornerShape(16.dp))
+            .drawBehind {
+                val stroke = 1.dp.toPx()
+                val radius = 16.dp.toPx()
+                drawRoundRect(
+                    color = strokColor,
+                    size = size,
+                    cornerRadius = CornerRadius(radius, radius),
+                    style = Stroke(width = stroke)
+                )
+            }
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 12.dp, start = 8.dp),
+            text = mediaType,
+            style = Theme.textStyle.label.medium,
+            color = Theme.color.textColors.title
+        )
+        Image(
+            modifier = Modifier
+                .padding(start = 42.dp, bottom = 8.dp)
+                .width(64.dp)
+                .height(71.dp)
+                .offset(y = (-8).dp),
+            painter = painterResource(mediaImgId),
+            contentDescription = "category img",
+        )
+
+
+    }
+
+
+}
+
+
+@ThemeAndLocalePreviews
+@Composable
+fun Preview1() {
+    CategoryCard(
+        modifier = Modifier,
+        "Western",
+        R.drawable.action_img
+    )
+}
+
+@ThemeAndLocalePreviews
+@Composable
+fun Preview2() {
+    AflamiTheme(isDarkTheme = true) {
+        CategoryCard(
+            modifier = Modifier,
+            "Western",
+            R.drawable.action_img
+        )
+    }
+}
