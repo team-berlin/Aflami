8e5f1b10095dd64d9cbf77740372ff360c99c639
nada <nadamansoursamir@gmail.com>
2025-07-03 14:57:45 +0300

    add string resource for arabic and new another font

--- app/src/main/java/com/berlin/aflami/MainActivity.kt
+++ app/src/main/java/com/berlin/aflami/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.berlin.aflami.ui.theme.theme.AflamiTheme
+import com.berlin.aflami.ui.theme.AflamiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
