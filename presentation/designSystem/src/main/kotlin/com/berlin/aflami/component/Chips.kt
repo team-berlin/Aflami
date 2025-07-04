5c3707bea4507ea8df5f54cf5ddc5f94e3f45427
Eman Abobakr <eman.abobakr@vodafone.com>
2025-07-04 17:37:14 +0300

    update hardcoded content description

--- presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/Chips.kt
+++ presentation/designSystem/src/main/kotlin/com/berlin/aflami/component/Chips.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.berlin.aflami.ui.theme.Theme
@@ -71,7 +72,7 @@ fun Chips(
         ) {
             Image(
                 painter = icon,
-                contentDescription = "search suggest photo",
+                contentDescription = stringResource(id = R.string.icon_cd),
                 modifier = Modifier
                     .size(24.dp)
                     .align(Alignment.Center),
