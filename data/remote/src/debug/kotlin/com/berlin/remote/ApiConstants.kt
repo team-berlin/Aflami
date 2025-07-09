fba5a08541fe0491d7aa838ed30ac9838ddac141
Ahmed Mahgoub <aed7862@gmail.com>
2025-07-09 18:36:07 +0300

    feat: add remote data source implementaion

---
+++ data/remote/src/debug/kotlin/com/berlin/remote/ApiConstants.kt
@@ -0,0 +1,11 @@
+package com.berlin.remote
+
+object ApiConstants  {
+
+    // endPoints
+    const val SEARCH_BY_COUNTRY = "discover/movie"
+
+    // parameters
+    const val WITH_ORIGIN_COUNTRY = "with_origin_country"
+
+}
