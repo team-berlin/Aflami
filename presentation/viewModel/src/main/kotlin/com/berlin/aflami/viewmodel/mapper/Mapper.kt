31c2a4e864a1b4f7ebfe37d8a794fd8014accac8
Ahmed Mahgoub <aed7862@gmail.com>
2025-07-10 03:04:49 +0300

    refactor: take first deciaml number from rating double to string

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mapper/Mapper.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mapper/Mapper.kt
@@ -1,14 +1,15 @@
 package com.berlin.aflami.viewmodel.mapper
 
+import android.annotation.SuppressLint
 import com.berlin.aflami.viewmodel.uistate.MovieUIState
 import com.berlin.entity.Movie
 
-
+@SuppressLint("DefaultLocale")
 fun Movie.toUIState(): MovieUIState {
     return MovieUIState(
         id = id,
         title = title,
-        rating = rating.toString(),
+        rating = String.format("%.1f", rating),
         releaseYear = releaseYear.year.toString(),
         description = description,
         genre = genre,
