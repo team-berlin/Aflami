74f9b2a33d3dcec3a19c9fa1a7dd2f9877240531
Eman Abobakr <eman.abobakr@vodafone.com>
2025-07-13 17:46:53 +0300

    add ui template

---
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/search/mediadetails/MediaDetailsScreen.kt
@@ -0,0 +1,24 @@
+package com.berlin.aflami.screens.search.mediadetails
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.berlin.aflami.ui.theme.AflamiTheme
+
+@Composable
+fun MediaDetailsScreen() {
+
+}
+
+@Composable
+fun MediaDetailsContent() {
+    Text("Hello Media!")
+}
+
+@Preview
+@Composable
+fun MediaDetailsContentPreview() {
+    AflamiTheme {
+        MediaDetailsContent()
+    }
+}
