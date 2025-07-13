9d55b45017296ffdae6bed6fafb3731cba0b7c4b
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-07-13 04:20:24 +0300

    extracted: interaction listeners to a separate package to be used across the whole app.

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mediaDetails/MediaInteractionListener.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mediaDetails/MediaInteractionListener.kt
@@ -1,32 +1,20 @@
 package com.berlin.aflami.viewmodel.mediaDetails
 
-interface MediaInteractionListener : AddToFavouriteInteractionListener,
+import com.berlin.aflami.viewmodel.reusableinteractionlistener.addtofavourite.AddToFavouriteInteractionListener
+import com.berlin.aflami.viewmodel.reusableinteractionlistener.createnewlist.CreateNewListInteractionListener
+import com.berlin.aflami.viewmodel.reusableinteractionlistener.rate.RateInteractionListener
+
+interface MediaInteractionListener :
     RateInteractionListener,
+    AddToFavouriteInteractionListener,
+    CreateNewListInteractionListener,
     ExtraMediaContentInteractionListener {
+
     fun onBackClicked()
     fun onPlayClicked(id: Long)
     fun onReadMoreDescriptionClicked(id: Long)
     fun onShowCastClicked(id: Long)
-}
 
-interface RateInteractionListener {
-    fun onRateMediaClicked(id: Long)
-    fun onSelectRateClicked(rate: Float)
-    fun onSubmitRateClicked(rate: Float)
-    fun onCancelRatingClicked()
-}
-
-interface AddToFavouriteInteractionListener : CreateNewListInteractionListener {
-    fun onAddMediaToFavouriteListClicked(favouriteListId: Int, mediaId: Int)
-    fun onSelectFavouriteList(favouriteListId: Int)
-    fun onCreateNewFavouriteListClicked()
-    fun onCancelAddingToFavouriteClicked()
-}
-
-interface CreateNewListInteractionListener {
-    fun onUpdateNewListTitle(newListTitle: String)
-    fun onCreateNewListClicked(listTitle: String)
-    fun onCancelCreatingNewListClicked()
 }
 
 interface ExtraMediaContentInteractionListener {
