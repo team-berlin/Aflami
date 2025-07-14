fd49dd3361eb1ebe743703557dfdeabad9ba83e9
NadeenRR <nadeenradwan0@gmail.com>
2025-07-14 10:51:13 +0300

    - feat: implement remote data source for movie and series

--- data/remote/src/debug/kotlin/com/berlin/remote/MovieDetailsRemoteDataSourceImpl.kt
+++ data/remote/src/debug/kotlin/com/berlin/remote/MovieDetailsRemoteDataSourceImpl.kt
@@ -1,6 +1,15 @@
 package com.berlin.remote
 
 import com.berlin.repository.datasource.remote.MovieDetailsRemoteDataSource
+import com.berlin.repository.datasource.remote.dto.ReviewResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class MovieDetailsRemoteDataSourceImpl : MovieDetailsRemoteDataSource {
+class MovieDetailsRemoteDataSourceImpl(
+    private val client: HttpClient
+): MovieDetailsRemoteDataSource {
+    override suspend fun getReviews(id: Long): ReviewResponse {
+        return client.get("movie/$id${ApiConstants.REVIEW}").body()
+    }
 }
--- data/remote/src/debug/kotlin/com/berlin/remote/SeriesDetailsRemoteDataSourceImpl.kt
+++ data/remote/src/debug/kotlin/com/berlin/remote/SeriesDetailsRemoteDataSourceImpl.kt
@@ -1,6 +1,15 @@
 package com.berlin.remote
 
 import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
+import com.berlin.repository.datasource.remote.dto.ReviewResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class SeriesDetailsRemoteDataSourceImpl : SeriesDetailsRemoteDataSource {
+class SeriesDetailsRemoteDataSourceImpl (
+    private val client: HttpClient
+): SeriesDetailsRemoteDataSource {
+    override suspend fun getReviews(id: Long): ReviewResponse {
+        return client.get("tv/$id${ApiConstants.REVIEW}").body()
+    }
 }
