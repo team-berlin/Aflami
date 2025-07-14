7d792449fc060bdb22222b07df52f36a175e91b5
nada <nadamansoursamir@gmail.com>
2025-07-14 04:34:19 +0300

    add repository implementation of movie and series cast details

--- data/repository/src/main/java/com/berlin/repository/MovieDetailsRepositoryImpl.kt
+++ data/repository/src/main/java/com/berlin/repository/MovieDetailsRepositoryImpl.kt
@@ -1,6 +1,16 @@
 package com.berlin.repository
 
+import com.berlin.entity.MediaCast
+import com.berlin.repository.datasource.remote.MovieDetailsRemoteDataSource
+import com.berlin.repository.mapper.toDomain
 import repository.MovieDetailsRepository
 
-class MovieDetailsRepositoryImpl : MovieDetailsRepository {
+class MovieDetailsRepositoryImpl(
+    private val remoteDataSource: MovieDetailsRemoteDataSource
+) : MovieDetailsRepository {
+    override suspend fun getMovieCastDetails(movieId: Long): List<MediaCast> {
+        return remoteDataSource.getMovieCastDetails(movieId).cast?.mapNotNull { castItemDto->
+            castItemDto?.toDomain()
+        }?: emptyList()
+    }
 }
--- data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
+++ data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
@@ -1,7 +1,17 @@
 package com.berlin.repository
 
+import com.berlin.entity.MediaCast
+import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
+import com.berlin.repository.mapper.toDomain
 import repository.SeriesDetailsRepository
 
-class SeriesDetailsRepositoryImpl : SeriesDetailsRepository {
+class SeriesDetailsRepositoryImpl(
+    private val remoteDataSource: SeriesDetailsRemoteDataSource
+) : SeriesDetailsRepository {
+    override suspend fun getSeriesCastDetails(seriesId: Long): List<MediaCast> {
+        return remoteDataSource.getSeriesCastDetails(seriesId).cast?.mapNotNull {castItemDto ->
+            castItemDto?.toDomain()
+        }?: emptyList()
+    }
 
 }
