37cde1a6bf737230a09b9327002a7fe109b61c70
NadeenRR <nadeenradwan0@gmail.com>
2025-07-14 10:51:52 +0300

    - feat: implement repository for movie and series

--- data/repository/src/main/java/com/berlin/repository/MovieDetailsRepositoryImpl.kt
+++ data/repository/src/main/java/com/berlin/repository/MovieDetailsRepositoryImpl.kt
@@ -1,6 +1,17 @@
 package com.berlin.repository
 
+import com.berlin.entity.Review
+import com.berlin.repository.datasource.remote.MovieDetailsRemoteDataSource
+import com.berlin.repository.mapper.toDomain
 import repository.MovieDetailsRepository
 
-class MovieDetailsRepositoryImpl : MovieDetailsRepository {
+class MovieDetailsRepositoryImpl(
+    private val remoteDataSource: MovieDetailsRemoteDataSource
+) : MovieDetailsRepository {
+    override suspend fun getReviews(id: Long): List<Review> {
+        return remoteDataSource.getReviews(id).results
+            ?.filterNotNull()
+            ?.map { reviewDto -> reviewDto.toDomain() }
+            ?: emptyList()
+    }
 }
--- data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
+++ data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
@@ -1,7 +1,18 @@
 package com.berlin.repository
 
+import com.berlin.entity.Review
+import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
+import com.berlin.repository.mapper.toDomain
 import repository.SeriesDetailsRepository
 
-class SeriesDetailsRepositoryImpl : SeriesDetailsRepository {
+class SeriesDetailsRepositoryImpl(
+    private val remoteDataSource: SeriesDetailsRemoteDataSource
+) : SeriesDetailsRepository {
+    override suspend fun getReviews(id: Long): List<Review> {
+        return remoteDataSource.getReviews(id).results
+            ?.filterNotNull()
+            ?.map {  reviewDto -> reviewDto.toDomain() }
+            ?: emptyList()
+    }
 
 }
