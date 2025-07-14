98182a0f945538f105be435dfa2723f63dd1d313
mohamedshemees <mohamedessampd@gmail.com>
2025-07-14 19:18:20 +0300

    Add steps to restore Google services and Firebase release key in CI workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,13 +46,6 @@ android {
     }
 
     buildTypes {
-        debug {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
         release {
             isMinifyEnabled = false
             proguardFiles(
