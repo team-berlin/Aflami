937d47a63c8cb172a582c75cf394bacfd62fe634
NadeenRR <nadeenradwan0@gmail.com>
2025-07-14 10:19:20 +0300

    - feat: add entity for review

---
+++ data/repository/src/main/java/com/berlin/repository/mapper/Review.kt
@@ -0,0 +1,22 @@
+package com.berlin.repository.mapper
+
+import com.berlin.entity.Review
+import com.berlin.repository.datasource.remote.dto.ReviewDto
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+fun ReviewDto.toDomain(): Review {
+    return Review(
+        id = this.id?.toLong() ?: 0L,
+        name = this.author ?: "",
+        userName = this.authorDetails?.userName ?: "",
+        avatarImage = this.authorDetails?.avatarPath ?: "",
+        rating = this.authorDetails?.rating ?: 0.0,
+        content = this.content ?: "",
+        date = (this.createdAt ?: Clock.System.now()
+            .toLocalDateTime(TimeZone.currentSystemDefault())
+            .date) as LocalDate,
+    )
+}
