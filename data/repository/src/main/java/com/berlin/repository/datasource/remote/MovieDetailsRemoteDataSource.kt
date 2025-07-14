47f0fecd77d8e1d700280d547455353d8a8bc519
NadeenRR <nadeenradwan0@gmail.com>
2025-07-14 10:34:24 +0300

    - feat: add getReviews fun. for movie and series in RemoteDataSource

--- data/repository/src/main/java/com/berlin/repository/datasource/remote/MovieDetailsRemoteDataSource.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/remote/MovieDetailsRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.berlin.repository.datasource.remote
 
+import com.berlin.repository.datasource.remote.dto.ReviewResponse
+
 interface MovieDetailsRemoteDataSource {
+    suspend fun getReviews(id: Long): ReviewResponse
 }
--- data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
@@ -1,4 +1,8 @@
 package com.berlin.repository.datasource.remote
 
+import com.berlin.repository.datasource.remote.dto.ReviewResponse
+
 interface SeriesDetailsRemoteDataSource {
+    suspend fun getReviews(id: Long): ReviewResponse
+
 }
