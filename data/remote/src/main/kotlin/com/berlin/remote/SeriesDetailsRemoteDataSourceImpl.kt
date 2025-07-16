9cf4ef2969817c66b38a15d6eac86be93da083f0
norhanadel <norhanadel367@gmail.com>
2025-07-16 16:01:45 +0300

    create fun getSeriesSimilar in SeriesDetailsRemoteDataSource

--- data/remote/src/main/kotlin/com/berlin/remote/SeriesDetailsRemoteDataSourceImpl.kt
+++ data/remote/src/main/kotlin/com/berlin/remote/SeriesDetailsRemoteDataSourceImpl.kt
@@ -1,6 +1,16 @@
 package com.berlin.remote
 
 import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
+import com.berlin.repository.datasource.remote.dto.TVShowResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class SeriesDetailsRemoteDataSourceImpl : SeriesDetailsRemoteDataSource {
+class SeriesDetailsRemoteDataSourceImpl(
+    private val client: HttpClient
+) : SeriesDetailsRemoteDataSource {
+    override suspend fun getSeriesSimilar(seriesId: Long): TVShowResponse {
+        return client.get(ApiConstants.SERIES_MORE_LIKE_THIS
+            .replace("{series_id}", seriesId.toString())).body()
+    }
 }
--- data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.berlin.repository.datasource.remote
 
+import com.berlin.repository.datasource.remote.dto.TVShowResponse
+
 interface SeriesDetailsRemoteDataSource {
+    suspend fun getSeriesSimilar(seriesId: Long): TVShowResponse
 }
