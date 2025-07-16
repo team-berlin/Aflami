921f69ced88cee9144d3a35060f445c718fbe5c7
norhanadel <norhanadel367@gmail.com>
2025-07-17 00:46:09 +0300

    edite inject to type HttpClient

--- app/src/main/java/com/berlin/aflami/di/dataSourceModule.kt
+++ app/src/main/java/com/berlin/aflami/di/dataSourceModule.kt
@@ -2,16 +2,19 @@ package com.berlin.aflami.di
 
 import com.berlin.local.dao.SearchDao
 import com.berlin.local.datasource.SearchLocalDataSourceImpl
+import com.berlin.remote.MovieDetailsRemoteDataSourceImpl
 import com.berlin.remote.SearchRemoteDataSourceImp
+import com.berlin.remote.SeriesDetailsRemoteDataSourceImpl
 import com.berlin.repository.datasource.local.SearchLocalDataSource
 import com.berlin.repository.datasource.remote.MovieDetailsRemoteDataSource
 import com.berlin.repository.datasource.remote.SearchRemoteDataSource
+import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 val dataSourceModule = module {
-    single<SearchRemoteDataSource> { SearchRemoteDataSourceImpl(get()) }
+    single<SearchRemoteDataSource> { SearchRemoteDataSourceImp(get<HttpClient>()) }
     single<SearchLocalDataSource> { SearchLocalDataSourceImpl(get<SearchDao>()) }
-    single<MovieDetailsRemoteDataSource> { MovieDetailsRemoteDataSourceImpl(get()) }
-    single<SeriesDetailsRemoteDataSource> { SeriesDetailsRemoteDataSourceImpl(get()) }
+    single<MovieDetailsRemoteDataSource> { MovieDetailsRemoteDataSourceImpl(get<HttpClient>()) }
+    single<SeriesDetailsRemoteDataSource> { SeriesDetailsRemoteDataSourceImpl(get<HttpClient>()) }
 }
