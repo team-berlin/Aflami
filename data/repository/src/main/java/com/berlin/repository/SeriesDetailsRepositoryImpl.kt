68e06e58c11e5c667a005da9c7857577ffda105f
norhanadel <norhanadel367@gmail.com>
2025-07-16 16:02:26 +0300

    create fun getSeriesSimilar in SeriesDetailsRepository

--- data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
+++ data/repository/src/main/java/com/berlin/repository/SeriesDetailsRepositoryImpl.kt
@@ -1,7 +1,16 @@
 package com.berlin.repository
 
+import com.berlin.entity.TVShow
+import com.berlin.repository.datasource.remote.SeriesDetailsRemoteDataSource
+import com.berlin.repository.mapper.toTVShow
 import repository.SeriesDetailsRepository
 
-class SeriesDetailsRepositoryImpl : SeriesDetailsRepository {
-
+class SeriesDetailsRepositoryImpl(
+    private val seriesDetailsRemoteDataSource: SeriesDetailsRemoteDataSource
+) : SeriesDetailsRepository {
+    override suspend fun getSeriesSimilar(seriesId: Long): List<TVShow> {
+        return seriesDetailsRemoteDataSource.getSeriesSimilar(seriesId).results?.mapNotNull { tvShowDto ->
+            tvShowDto?.toTVShow()
+        } ?: emptyList()
+    }
 }
--- domain/usecase/src/main/kotlin/repository/SeriesDetailsRepository.kt
+++ domain/usecase/src/main/kotlin/repository/SeriesDetailsRepository.kt
@@ -1,5 +1,7 @@
 package repository
 
-interface SeriesDetailsRepository {
+import com.berlin.entity.TVShow
 
+interface SeriesDetailsRepository {
+    suspend fun getSeriesSimilar(seriesId:Long):List<TVShow>
 }
