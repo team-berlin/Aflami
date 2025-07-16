dc2999995db7b8fb862afa360735c0906ed71b25
NadeenRR <nadeenradwan0@gmail.com>
2025-07-16 12:51:35 +0300

    - feat: update MediaDetailsScreen

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/search/mediadetails/MediaDetailsScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/search/mediadetails/MediaDetailsScreen.kt
@@ -1,43 +1,48 @@
 package com.berlin.aflami.screens.search.mediadetails
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.berlin.aflami.screens.search.components.ErrorMessage
-import com.berlin.aflami.screens.search.components.Loading
+import com.berlin.aflami.component.Chips
 import com.berlin.aflami.ui.theme.AflamiTheme
-import com.berlin.aflami.ui.theme.Theme
 import com.berlin.aflami.viewmodel.mediadetails.MediaDetailsViewmodel
+import com.berlin.aflami.viewmodel.mediadetails.MovieDetailsTabs
 import com.berlin.aflami.viewmodel.review.ReviewState
+import com.berlin.designsystem.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MediaDetailsScreen(
-    navController : NavController,
+    navController: NavController,
     viewModel: MediaDetailsViewmodel = koinViewModel()
 ) {
     val review by viewModel.reviewsUiState.collectAsState()
+    val tabSelected by viewModel.tabSelectedUiState.collectAsState()
 
-    // I know we need change this when edit navigation
-    LaunchedEffect(Unit) {
-        viewModel.getReviews(id = 79L, mediaType = MediaDetailsViewmodel.MediaType.SERIES)
-    }
 
     MediaDetailsContent(
         reviewState = review,
-        onToggleExpand = { viewModel.onReadMoreDescriptionClicked(id = 79L) },
-        isExpanded = viewModel.isDescriptionExpanded(id = 79L)
+        onToggleExpand = { viewModel.onReadMoreDescriptionClicked(id = TODO()) },
+        isExpanded = viewModel.isDescriptionExpanded(id = TODO()),
+        isSelectedTab = tabSelected.tab,
+        onChipClick = { tab ->
+            viewModel.toggleMovieDetailsTab(
+                tab = tab,
+                mediaId = TODO(),
+                mediaType = TODO()
+            )
+        }
     )
 
 }
@@ -47,47 +52,32 @@ fun MediaDetailsContent(
     reviewState: ReviewState,
     isExpanded: Boolean,
     onToggleExpand: () -> Unit,
+    isSelectedTab: MovieDetailsTabs,
+    onChipClick: (MovieDetailsTabs) -> Unit
 ) {
 
-    when (reviewState) {
-
-        is ReviewState.Reviewing.Loading -> {
-            Loading(Modifier)
+    LazyRow(
+        modifier = Modifier
+            .height(96.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(MovieDetailsTabs.entries) { tab ->
+            Chips(
+                title = stringResource(movieDetailsTabsMapper(tab)),
+                icon = painterResource(getMovieDetailsTabsIcon(tab)),
+                isSelected = tab == isSelectedTab,
+                onClick = { onChipClick(tab) }
+            )
         }
+    }
 
-        is ReviewState.Reviewing.Success -> {
-            val reviews = reviewState.data
-            LazyColumn {
-                itemsIndexed(reviews) { index, review ->
-                    val isLast = index == reviews.lastIndex
-                    ReviewItem(
-                        review = review,
-                        isLastItem = isLast,
-                        isExpanded =isExpanded,
-                        onToggleExpand = onToggleExpand
-                    )
-                }
-            }
-        }
-
-        is ReviewState.NoReviewFound -> {
-            Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    "No Reviews Found",
-                    color = Theme.color.textColors.title,
-                    style = Theme.textStyle.title.medium,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-
-        is ReviewState.Reviewing.Error -> {
-            ErrorMessage(Modifier, reviewState.errorMessage)
-        }
-
+    if (isSelectedTab == MovieDetailsTabs.REVIEWS) {
+        ReviewSection(
+            reviewState = reviewState,
+            isExpanded = isExpanded,
+            onToggleExpand = onToggleExpand
+        )
     }
 }
 
@@ -98,7 +88,27 @@ fun MediaDetailsContentPreview() {
         MediaDetailsContent(
             reviewState = TODO(),
             isExpanded = TODO(),
-            onToggleExpand = TODO()
+            onToggleExpand = TODO(),
+            isSelectedTab = TODO(),
+            onChipClick = TODO()
         )
+    }
+}
+
+private fun movieDetailsTabsMapper(tab: MovieDetailsTabs): Int {
+    return when (tab) {
+        MovieDetailsTabs.MORE_LIKE_THIS -> R.string.more_like_this
+        MovieDetailsTabs.REVIEWS -> R.string.reviews
+        MovieDetailsTabs.GALLERY -> R.string.gallery
+        MovieDetailsTabs.COMPANY_PRODUCTION -> R.string.company_production
+    }
+}
+
+private fun getMovieDetailsTabsIcon(tab: MovieDetailsTabs): Int {
+    return when (tab) {
+        MovieDetailsTabs.MORE_LIKE_THIS -> com.berlin.ui.R.drawable.camera_video
+        MovieDetailsTabs.REVIEWS -> R.drawable.star
+        MovieDetailsTabs.GALLERY -> com.berlin.ui.R.drawable.album
+        MovieDetailsTabs.COMPANY_PRODUCTION -> com.berlin.ui.R.drawable.city
     }
 }
