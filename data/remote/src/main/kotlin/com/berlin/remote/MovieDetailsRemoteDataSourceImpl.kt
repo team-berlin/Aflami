30d6de4e8478060484421f40cb5747c7e737107b
norhanadel <norhanadel367@gmail.com>
2025-07-16 16:01:26 +0300

    create fun getMovieSimilar in MovieDetailsRemoteDataSource

--- data/remote/src/main/kotlin/com/berlin/remote/MovieDetailsRemoteDataSourceImpl.kt
+++ data/remote/src/main/kotlin/com/berlin/remote/MovieDetailsRemoteDataSourceImpl.kt
@@ -1,6 +1,16 @@
 package com.berlin.remote
 
 import com.berlin.repository.datasource.remote.MovieDetailsRemoteDataSource
+import com.berlin.repository.datasource.remote.dto.MovieResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-class MovieDetailsRemoteDataSourceImpl : MovieDetailsRemoteDataSource {
+class MovieDetailsRemoteDataSourceImpl(
+    private val client: HttpClient
+) : MovieDetailsRemoteDataSource {
+    override suspend fun getMovieSimilar(movieId: Long): MovieResponse {
+        return client.get(ApiConstants.MOVIE_MORE_LIKE_THIS
+            .replace("{movie_id}", movieId.toString())).body()
+    }
 }
--- data/repository/src/main/java/com/berlin/repository/datasource/remote/MovieDetailsRemoteDataSource.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/remote/MovieDetailsRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.berlin.repository.datasource.remote
 
+import com.berlin.repository.datasource.remote.dto.MovieResponse
+
 interface MovieDetailsRemoteDataSource {
+    suspend fun getMovieSimilar(movieId: Long): MovieResponse
 }
