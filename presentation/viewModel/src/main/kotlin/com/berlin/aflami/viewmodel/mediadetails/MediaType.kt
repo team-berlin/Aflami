ad585b9353d700300f072d813a3a2161ad28b236
NadeenRR <nadeenradwan0@gmail.com>
2025-07-16 12:50:47 +0300

    - feat: add media type file

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mediadetails/MediaType.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/mediadetails/MediaType.kt
@@ -1,5 +1,17 @@
 package com.berlin.aflami.viewmodel.mediadetails
 
+
+data class MovieDetailsTabsUiState(
+    val tab: MovieDetailsTabs = MovieDetailsTabs.REVIEWS,
+    val isSelected: Boolean = false,
+)
 enum class MediaType {
     MOVIE, SERIES
+}
+
+enum class MovieDetailsTabs() {
+    MORE_LIKE_THIS,
+    REVIEWS,
+    GALLERY,
+    COMPANY_PRODUCTION
 }
