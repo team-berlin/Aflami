3e95e2bdf57155f0ac31fdd8f8acbb33ac83458a
Ahmed Mahgoub <aed7862@gmail.com>
2025-07-17 06:22:11 +0300

    refactor: rename search by country route function

--- app/src/main/java/com/berlin/aflami/navigation/routes/SearchByCountryRoute.kt
+++ app/src/main/java/com/berlin/aflami/navigation/routes/SearchByCountryRoute.kt
@@ -3,13 +3,13 @@ package com.berlin.aflami.navigation.routes
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.berlin.aflami.screens.search.worldtour.WorldTourScreen
+import com.berlin.aflami.screens.search.worldtour.SearchByCountryScreen
 import com.example.navigation.Destination.WorldTourScreen
 
-fun NavGraphBuilder.worldTourRoute(
+fun NavGraphBuilder.searchByCountryRoute(
     navController: NavController
 ) {
     composable(route = WorldTourScreen.route) {
-        WorldTourScreen(navController = navController)
+        SearchByCountryScreen(navController = navController)
     }
 }
