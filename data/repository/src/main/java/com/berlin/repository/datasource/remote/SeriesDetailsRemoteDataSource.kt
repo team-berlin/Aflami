e6f10fa2d2142861022cd513b8cbdb2b232e1dd7
norhanadel <norhanadel367@gmail.com>
2025-07-17 07:05:49 +0300

    create fun getEpisodeSeasonSeries in SeriesDetailsRemoteDataSource

--- data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/remote/SeriesDetailsRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.berlin.repository.datasource.remote
 
+import com.berlin.repository.datasource.remote.dto.details.EpisodeResponse
+
 interface SeriesDetailsRemoteDataSource {
+    suspend fun getEpisodeSeasonSeries(seriesId: Long, seasonNumber: Int): EpisodeResponse
 }
