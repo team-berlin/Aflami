881ded65f4f0c8e9e4a02868634c62e387c1102f
Eman Abobakr <eman.abobakr@vodafone.com>
2025-07-18 04:31:54 +0300

    add MoreLikeThisSection

---
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mediadetails/components/MoreLikeThisSection.kt
@@ -0,0 +1,41 @@
+package com.berlin.aflami.screens.mediadetails.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.berlin.aflami.component.MediaCard
+import com.berlin.aflami.viewmodel.uistate.MediaType
+import com.berlin.aflami.viewmodel.uistate.MediaUiState
+import com.berlin.ui.R
+
+@Composable
+fun MoreLikeThisSection(
+    mediaList: List<MediaUiState>,
+    mediaType: MediaType
+) {
+  LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(mediaList) { media ->
+            MediaCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(196.dp),
+                mediaImg = media.poster,
+                title = media.title,
+                typeOfMedia = if (mediaType == MediaType.MOVIE) R.string.movie.toString() else R.string.Tv_Show.toString(),
+                date = media.releaseYear.substringBefore("-"),
+                rating = media.rating
+            )
+        }
+    }
+}
