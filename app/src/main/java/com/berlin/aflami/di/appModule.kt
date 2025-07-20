c62f7c1dde970e2237429fcc8daab708367ea174
norhanadel <norhanadel367@gmail.com>
2025-07-20 22:16:28 +0300

    separated network module add OkHttpClient , Retrofit

--- app/src/main/java/com/berlin/aflami/di/appModule.kt
+++ app/src/main/java/com/berlin/aflami/di/appModule.kt
@@ -1,49 +1,11 @@
 package com.berlin.aflami.di
 
 import androidx.room.Room
-import com.berlin.aflami.BuildConfig
-import com.berlin.aflami.util.ApiKeyInterceptor
 import com.berlin.local.SearchDatabase
-import com.berlin.remote.network.MovieApiService
-import com.berlin.remote.network.SearchApiService
-import com.berlin.remote.network.TVShowApiService
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import retrofit2.Retrofit
 
 val appModule = module {
-
-    single {
-        val logging = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(ApiKeyInterceptor())
-            .addInterceptor(logging)
-            .build()
-
-        val json = Json {
-            ignoreUnknownKeys = true
-            classDiscriminator = "media_type"
-        }
-
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .client(client)
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-            .build()
-    }
-
-    single { get<Retrofit>().create(MovieApiService::class.java) }
-    single { get<Retrofit>().create(TVShowApiService::class.java) }
-    single { get<Retrofit>().create(SearchApiService::class.java) }
-
     single {
         Room.databaseBuilder(
             androidContext(),
