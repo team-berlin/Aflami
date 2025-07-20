22eb2836ad5b05265189cb3f37465ad39f94190c
norhanadel <norhanadel367@gmail.com>
2025-07-20 12:40:00 +0300

    edite in client Retrofit

--- app/src/main/java/com/berlin/aflami/di/appModule.kt
+++ app/src/main/java/com/berlin/aflami/di/appModule.kt
@@ -9,24 +9,40 @@ import com.berlin.remote.network.TVShowApiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val appModule = module {
 
-    // JSON setup for Kotlinx Serialization
     single {
-        Json {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val url = original.url.newBuilder()
+                    .addQueryParameter("api_key", BuildConfig.API_KEY)
+                    .build()
+                val request = original.newBuilder().url(url).build()
+                chain.proceed(request)
+            }
+            .addInterceptor(logging)
+            .build()
+
+        val json = Json {
             ignoreUnknownKeys = true
             classDiscriminator = "media_type"
         }
-    }
 
-    single {
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(get<Json>().asConverterFactory("application/json".toMediaType()))
+            .client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
 
