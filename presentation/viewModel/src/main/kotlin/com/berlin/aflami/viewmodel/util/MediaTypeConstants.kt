d0065aef343a3ef27f5d7b29c5eec212abfb478e
khaatteerr <hamdikhater68@gmail.com>
2025-07-21 02:57:32 +0300

    remove unused MediaTypeConstants object

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/util/MediaTypeConstants.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/util/MediaTypeConstants.kt
@@ -1,12 +1,6 @@
 package com.berlin.aflami.viewmodel.util
 
-object MediaTypeConstants {
-    const val MOVIE="MOVIE"
-    const val TV_SHOW="TV"
-
-}
 enum class MediaType{
     MOVIE,
     TV_SHOW
-
 }
