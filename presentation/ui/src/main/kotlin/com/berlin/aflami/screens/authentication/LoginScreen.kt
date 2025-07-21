0f91b3959729877f17a7d8b6a3ca5994158e3471
norhanadel <norhanadel367@gmail.com>
2025-07-21 23:44:47 +0300

    improve login screen

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/authentication/LoginScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/authentication/LoginScreen.kt
@@ -1,7 +1,5 @@
 package com.berlin.aflami.screens.authentication
 
-import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -14,7 +12,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,30 +25,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.berlin.aflami.component.IconButton
 import com.berlin.aflami.component.PrimaryButton
 import com.berlin.aflami.component.TextField
 import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
-import com.berlin.ui.R
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.zIndex
-import com.berlin.aflami.component.ThemeAndLocalePreviews
+import com.berlin.aflami.component.IconButton
+import com.berlin.ui.R
 
 @Composable
 fun LoginScreen() {
+
     LoginContent()
 }
 
+
 @Composable
 fun LoginContent() {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var passwordError by remember { mutableStateOf(false) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -62,23 +56,25 @@ fun LoginContent() {
             )
             .padding(horizontal = 12.dp),
     ) {
-        Column(
+        Column (
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 24.dp),
-        ) {
-            LoginHeader()
-            LoginForm(
-                username = username,
-                password = password,
-                passwordError = passwordError,
-                onUsernameChange = { username = it },
-                onPasswordChange = { password = it },
-                onPasswordErrorChange = { passwordError = it }
-            )
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+            ) {
+                LoginLogo()
+                WelcomeText()
+                FormLogin()
+            }
             Spacer(modifier = Modifier.height(48.dp))
+
             LoginButtons()
         }
+
         Row(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
@@ -100,68 +96,65 @@ fun LoginContent() {
                     .padding(start = 4.dp)
             )
         }
+
     }
+}
+@Composable
+fun LoginLogo() {
+    IconButton(
+        painter = painterResource(com.berlin.designsystem.R.drawable.home_logo),
+        contentDescription = "search",
+        modifier = Modifier
+            .size(48.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(
+                color = Theme.color.primaryVariant,
+            )
+            .border(
+                width = 1.dp,
+                color = Theme.color.stroke,
+                shape = RoundedCornerShape(12.dp)
+            ),
+        paddingValues = PaddingValues(8.dp),
+        withBorder = true,
+        containerColor = Theme.color.primaryVariant,
+        onClick = {}
+    )
 }
 
 @Composable
-fun LoginHeader() {
+fun WelcomeText() {
     Column(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(24.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        IconButton(
-            painter = painterResource(com.berlin.designsystem.R.drawable.home_logo),
-            contentDescription = "search",
-            modifier = Modifier
-                .size(48.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(color = Theme.color.primaryVariant)
-                .border(
-                    width = 1.dp,
-                    color = Theme.color.stroke,
-                    shape = RoundedCornerShape(12.dp)
-                ),
-            paddingValues = PaddingValues(8.dp),
-            withBorder = true,
-            containerColor = Theme.color.primaryVariant,
-            onClick = {}
+        Text(
+            text = stringResource(R.string.welcome_back),
+            color = Theme.color.textColors.title,
+            style = Theme.textStyle.title.medium
         )
-        Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            Text(
-                text = stringResource(R.string.welcome_back),
-                color = Theme.color.textColors.title,
-                style = Theme.textStyle.title.medium
-            )
-            Text(
-                text = stringResource(R.string.please_enter_info),
-                color = Theme.color.textColors.body,
-                style = Theme.textStyle.body.medium
-            )
-        }
+        Text(
+            text = stringResource(R.string.please_enter_info),
+            color = Theme.color.textColors.body,
+            style = Theme.textStyle.body.medium
+        )
     }
 }
 
 @Composable
-fun LoginForm(
-    username: String,
-    password: String,
-    passwordError: Boolean,
-    onUsernameChange: (String) -> Unit,
-    onPasswordChange: (String) -> Unit,
-    onPasswordErrorChange: (Boolean) -> Unit
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
+fun FormLogin() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column{
         TextField(
             text = username,
             leadingIcon = R.drawable.user_square,
             hintText = stringResource(R.string.username),
-            onValueChange = onUsernameChange,
+            onValueChange = { username = it },
             modifier = Modifier.fillMaxWidth()
         )
+        var passwordError by remember { mutableStateOf(false) }
+        Spacer(modifier = Modifier.height(12.dp))
+
         TextField(
             text = password,
             hintText = stringResource(R.string.password),
@@ -169,11 +162,13 @@ fun LoginForm(
             isError = passwordError,
             errorMessage = if (passwordError) stringResource(R.string.incorrect_password) else "",
             isObscured = true,
-            onValueChange = onPasswordChange,
+            onValueChange = { password = it },
             trailingIcon = R.drawable.heroicons_outline,
-            onTrailingClick = { onPasswordErrorChange(!passwordError) },
+            onTrailingClick = { },
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(8.dp))
+
         Text(
             text = stringResource(R.string.forgot_password),
             style = Theme.textStyle.body.small,
@@ -200,28 +195,30 @@ fun LoginButtons() {
             gradientColor = Theme.color.primaryButton
         ) {
             Text(
-                text = stringResource(R.string.login),
+                stringResource(R.string.login),
                 style = Theme.textStyle.label.large,
                 color = Theme.color.textColors.onPrimary
             )
         }
+
         PrimaryButton(
-            onClick = {},
+            onClick = {  },
             containerColor = Theme.color.primaryVariant,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(56.dp),
         ) {
             Text(
-                text = stringResource(R.string.continue_as_guest),
+                stringResource(R.string.continue_as_guest),
                 style = Theme.textStyle.label.large,
                 color = Theme.color.primary
             )
         }
     }
 }
-@Preview(
-    showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, backgroundColor = 0xFF0D090B, heightDp = 700, widthDp = 360)
+
+
+@Preview(showBackground = true, heightDp = 800, widthDp = 360)
 @Composable
 fun LoginScreenPreview() {
     AflamiTheme(isDarkTheme = false) {
