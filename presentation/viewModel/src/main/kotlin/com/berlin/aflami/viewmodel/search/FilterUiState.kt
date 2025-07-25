3161947965b3a45a45ac78e963c807a82cb63412
norhanadel <norhanadel367@gmail.com>
2025-07-25 05:58:53 +0300

    refactor issue FilterItemUiState

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/search/FilterUiState.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/search/FilterUiState.kt
@@ -1,5 +1,7 @@
 package com.berlin.aflami.viewmodel.search
 
+import com.berlin.aflami.viewmodel.search.FilterItemUiState.Companion.defaultGenres
+
 data class GenreUiState(
     val id: Int,
     val name: String,
@@ -9,12 +11,13 @@ data class GenreUiState(
 data class FilterMediaSelected(
     val selectedRating: Float = 1f,
     val selectedGenres: Int = -1,
-)
+    val genreUiStates: List<GenreUiState> = defaultGenres,
+
+    )
 
 data class FilterItemUiState(
     val filterTvShowSelected: FilterMediaSelected = FilterMediaSelected(),
     val filterMovieSelected: FilterMediaSelected = FilterMediaSelected(),
-    val genreUiStates: List<GenreUiState> = defaultGenres,
     val isLoading: Boolean = false
 ) {
     companion object {
