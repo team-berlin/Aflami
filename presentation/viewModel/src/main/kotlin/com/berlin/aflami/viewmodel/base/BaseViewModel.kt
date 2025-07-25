eabe0d5798d3811d82f8bcec7573298ef1a044cb
nada <nadamansoursamir@gmail.com>
2025-07-25 15:56:34 +0300

    remove comment

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/base/BaseViewModel.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/base/BaseViewModel.kt
@@ -63,7 +63,6 @@ abstract class BaseViewModel<S, E>(
             } catch (e: Exception) {
                 onError(ErrorUiState(e.message.toString()))
             } catch (exception: Exception) {
-                //Log.e("CATCH", "tryToCall: ", exception)
                 onError(ErrorUiState(exception.message.toString()))
             }
         }
