42c23ec1f0246e9a9b5b0322c21c5d2ceaec37f3
NadeenRR <nadeenradwan0@gmail.com>
2025-07-31 14:26:12 +0300

    feat: add splash screen and update theme
    - Added `androidx.core:core-splashscreen` dependency.
    - Created `themes.xml` and `colors.xml` for night mode in `presentation/designSystem`.
    - Defined `Theme.Aflami.Splash` style in both `values/themes.xml` and `values-night/themes.xml` to customize the splash screen background and icon.
    - Applied the splash screen theme to `MainActivity` in `AndroidManifest.xml`.
    - Updated `background_color` in `values/colors.xml`.
    - Minor UI adjustment in `HomeScreen.kt`.

--- presentation/designSystem/build.gradle.kts
+++ presentation/designSystem/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     debugImplementation(libs.bundles.composeUiDebug)
     implementation(libs.androidx.navigation)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
 
     implementation(project(":presentation:safeImageViewer"))
 
