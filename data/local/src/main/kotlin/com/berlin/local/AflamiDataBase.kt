423a046edeeebf34461cdeafcf698a9fb2c36368
norhanadel <norhanadel367@gmail.com>
2025-08-04 15:21:47 +0300

    create MovieHomeEntity::class TVShowHomeEntity::class

--- data/local/src/main/kotlin/com/berlin/local/AflamiDataBase.kt
+++ data/local/src/main/kotlin/com/berlin/local/AflamiDataBase.kt
@@ -9,11 +9,14 @@ import com.berlin.repository.datasource.Converters
 import com.berlin.local.dao.CategoriesPreferencesDao
 import com.berlin.local.dao.ContinueWatchingDao
 import com.berlin.local.dao.GenreDao
+import com.berlin.local.dao.MediaHomeDao
 import com.berlin.local.dao.RecentHistoryDao
 import com.berlin.local.dao.SearchDao
 import com.berlin.repository.datasource.local.dto.CategoriesPreferencesEntity
 
 import com.berlin.repository.datasource.local.dto.GenreEntity
+import com.berlin.repository.datasource.local.dto.MovieHomeEntity
+import com.berlin.repository.datasource.local.dto.TVShowHomeEntity
 import com.berlin.repository.datasource.local.dto.RecentHistoryEntity
 import com.berlin.repository.datasource.local.dto.RecentlyWatchedMovieEntity
 import com.berlin.repository.datasource.local.dto.RecentlyWatchedTvShowEntity
@@ -26,9 +29,10 @@ import com.berlin.repository.datasource.local.dto.SearchingEntity
         RecentHistoryEntity::class,
         CategoriesPreferencesEntity::class,
         RecentlyWatchedMovieEntity::class,
-        RecentlyWatchedTvShowEntity::class
-        ,
-        GenreEntity::class
+        RecentlyWatchedTvShowEntity::class,
+        GenreEntity::class,
+        MovieHomeEntity::class,
+        TVShowHomeEntity::class
     ], version = 1, exportSchema = false
 )
 abstract class AflamiDatabase : RoomDatabase() {
@@ -37,6 +41,7 @@ abstract class AflamiDatabase : RoomDatabase() {
     abstract fun categoriesPreferencesDao(): CategoriesPreferencesDao
     abstract fun continueWatchingDao(): ContinueWatchingDao
     abstract fun genreDao(): GenreDao
+    abstract fun mediaHomeDao(): MediaHomeDao
 
     companion object {
         private const val DATABASE_NAME = "Aflami_Database"
