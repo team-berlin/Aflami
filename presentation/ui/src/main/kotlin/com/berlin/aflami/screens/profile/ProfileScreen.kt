60876299de4084488bcc9f5123f3fddc5aa787e8
khaatteerr <hamdikhater68@gmail.com>
2025-08-05 22:40:00 +0300

    add profile components into profile screen

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/profile/ProfileScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/profile/ProfileScreen.kt
@@ -3,28 +3,60 @@ package com.berlin.aflami.screens.profile
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.berlin.aflami.component.ThemeAndLocalePreviews
+import com.berlin.aflami.screens.profile.components.ProfileSection
+import com.berlin.aflami.screens.profile.components.SettingSection
+import com.berlin.aflami.screens.profile.components.WatchHistoryRatingSection
+import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
-
+import com.berlin.ui.R
 
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen() {
     ProfileContent()
 
 }
 
 @Composable
-fun ProfileContent(){
+fun ProfileContent() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
             .background(Theme.color.surface)
+
     )
-    { Text(text = "Profile Screen") }
+    {
+        ProfileSection(userAvatar = "", userName = "", painterResource(R.drawable.profile_cover))
+        Spacer(modifier = Modifier.height(24.dp))
+        WatchHistoryRatingSection()
+        Spacer(modifier = Modifier.height(24.dp))
+        HorizontalDivider(thickness = 1.dp, color = Theme.color.stroke)
+        Spacer(modifier = Modifier.height(24.dp))
+        SettingSection(
+            isLanguageEN = true,
+            isDarkThemeEnabled = true,
+        )
+
+    }
 }
+
+
+@ThemeAndLocalePreviews
+@Composable
+fun PreviewProfileSection() {
+    AflamiTheme {
+        ProfileScreen()
+    }
+}
+
