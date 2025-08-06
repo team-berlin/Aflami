4004bdccb3d56a901293446ca5bd147e616e1552
norhanadel <norhanadel367@gmail.com>
2025-08-06 06:37:03 +0300

    rename some fun not clear

--- data/repository/src/main/java/com/berlin/repository/datasource/local/dto/MediaEntity.kt
+++ data/repository/src/main/java/com/berlin/repository/datasource/local/dto/MediaEntity.kt
@@ -2,7 +2,7 @@ package com.berlin.repository.datasource.local.dto
 
 import androidx.room.Entity
 
-@Entity(tableName = "Movie_Home", primaryKeys = ["id", "type"])
+@Entity(tableName = "Movie_Home", primaryKeys = ["id", "sectionHome"])
 data class MovieHomeEntity(
     val id: Long,
     val sectionHome: SectionHome,
@@ -14,7 +14,7 @@ data class MovieHomeEntity(
     val addedAt: Long = System.currentTimeMillis()
 )
 
-@Entity(tableName = "TVShow_Home", primaryKeys = ["id", "type"])
+@Entity(tableName = "TVShow_Home", primaryKeys = ["id", "sectionHome"])
 data class TVShowHomeEntity(
     val id: Long,
     val sectionHome: SectionHome,
