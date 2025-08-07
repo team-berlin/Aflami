b43a11e8ffb4d5d78ded7fb8b59f2522f093d977
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-08-07 08:39:51 +0300

    feat: refactor ListDetailsScreen to integrate DeleteListDialog and improve component structure

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/listdetails/ListDetailsScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/listdetails/ListDetailsScreen.kt
@@ -3,53 +3,37 @@ package com.berlin.aflami.screens.listdetails
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemKey
 import com.berlin.aflami.component.CircularProgressIndicator
 import com.berlin.aflami.component.DefaultBar
-import com.berlin.aflami.component.IconButton
-import com.berlin.aflami.component.MediaCard
-import com.berlin.aflami.component.PrimaryButton
 import com.berlin.aflami.screens.NoInternetConnectionPlaceholder
+import com.berlin.aflami.screens.listdetails.component.DeleteListDialog
+import com.berlin.aflami.screens.listdetails.component.MoviesListItem
 import com.berlin.aflami.screens.search.components.NoDataContainer
+import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
 import com.berlin.aflami.viewmodel.listDetails.ListDetailsScreenInteractionListener
 import com.berlin.aflami.viewmodel.listDetails.ListDetailsScreenState
 import com.berlin.aflami.viewmodel.listDetails.ListDetailsScreenViewModel
-import com.berlin.aflami.viewmodel.shareduistate.MovieUiState
 import com.berlin.ui.R
 import kotlinx.coroutines.flow.collectLatest
 
@@ -91,8 +75,8 @@ private fun ListDetailsContent(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp, 8.dp),
-            firstOption = painterResource(com.berlin.ui.R.drawable.edit),
-            lastOption = painterResource(com.berlin.ui.R.drawable.delete),
+            firstOption = painterResource(R.drawable.edit),
+            lastOption = painterResource(R.drawable.delete),
             onFirstOptionClicked = {},
             onLastOptionClicked = {},
         )
@@ -149,119 +133,33 @@ private fun ListDetailsContent(
             onDismiss = listener::onDeleteDialogDismiss, onConfirm = listener::onDeleteConfirmed
         )
     }
-
 }
 
+@Preview
 @Composable
-fun MoviesListItem(
-    movies: LazyPagingItems<MovieUiState>,
-    modifier: Modifier = Modifier,
-    onClickMovie: (Long) -> Unit,
-    onClickDislikeItem: (Long) -> Unit = { }
-) {
-    val itemState = rememberLazyGridState()
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 160.dp),
-        state = itemState,
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(
-            count = movies.itemCount, key = movies.itemKey { it.id }) { index ->
-            val movie = movies[index] ?: return@items
-            Box {
-                MediaCard(
-                    mediaImg = movie.posterUrl,
-                    title = movie.title,
-                    date = movie.releaseDate,
-                    rating = movie.rating,
-                    typeOfMedia = stringResource(R.string.movie),
-                    onClick = { onClickMovie(movie.id) })
+private fun PreviewListDetailsContent() {
+    AflamiTheme {
+        ListDetailsContent(
+            state = ListDetailsScreenState(),
+            listener = object : ListDetailsScreenInteractionListener {
+                override fun onMovieCardClicked(id: Long) {}
+                override fun onRemoveMovieClicked(id: Long) {}
+                override fun onBackClicked() {
+                    TODO("Not yet implemented")
+                }
 
-                IconButton(
-                    modifier = Modifier
-                        .padding(start = 4.dp, top = 4.dp)
-                        .size(32.dp),
-                    paddingValues = PaddingValues(6.dp),
-                    painter = painterResource(com.berlin.ui.R.drawable.heart),
-                    contentDescription = null,
-                    onClick = { onClickDislikeItem(movie.id) },
-                )
+                override fun onRenameClicked(listId: Int) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDeleteIconClicked(listId: Int) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDeleteDialogDismiss() {}
+                override fun onDeleteConfirmed() {}
             }
-        }
-    }
-}
-
-@Composable
-private fun DeleteListDialog(
-    modifier: Modifier = Modifier,
-    onDismiss: () -> Unit,
-    onConfirm: () -> Unit,
-) {
-    Dialog(onDismissRequest = onDismiss) {
-        Column(
-            modifier = Modifier.padding(12.dp), horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            DialogTitleBar(
-                titleResource = R.string.delete_list,
-                modifier = Modifier.fillMaxWidth(),
-                onDismiss = onDismiss
-            )
-            Image(
-                painter = painterResource(R.drawable.deletealert),
-                contentDescription = stringResource(R.string.delete_list),
-                modifier = Modifier.height(100.dp),
-                contentScale = ContentScale.FillHeight
-            )
-
-            Text(
-                modifier = Modifier.padding(top = 12.dp),
-                text = stringResource(R.string.delete_confirm),
-                style = Theme.textStyle.title.small,
-                color = Theme.color.textColors.body,
-                textAlign = TextAlign.Center
-            )
-
-            PrimaryButton(
-                onClick = onConfirm,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
-                containerColor = Theme.color.primaryVariant
-            ) {
-                Text(
-                    stringResource(R.string.delete),
-                    style = Theme.textStyle.label.large,
-                    color = Theme.color.primary
-                )
-            }
-
-        }
-    }
-}
-
-
-@Composable
-private fun DialogTitleBar(
-    titleResource: Int, modifier: Modifier = Modifier, onDismiss: () -> Unit
-) {
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = stringResource(titleResource),
-            style = Theme.textStyle.title.large,
-            color = Theme.color.textColors.title
-        )
-        IconButton(
-            painter = painterResource(com.berlin.designsystem.R.drawable.cancel),
-            contentDescription = "Cancel",
-            onClick = { onDismiss() },
-            tint = Theme.color.textColors.title
         )
     }
+
 }
