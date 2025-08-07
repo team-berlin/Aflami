8b266c7b040184f84aa6b7b939c6fbb09a463850
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-08-07 07:10:10 +0300

    feat: implement ProfileScreen and integrate ProfileViewModel

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/profile/ProfileScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/profile/ProfileScreen.kt
@@ -8,25 +8,77 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.berlin.aflami.component.ThemeAndLocalePreviews
+import com.berlin.aflami.screens.profile.components.OptionsDialog
 import com.berlin.aflami.screens.profile.components.ProfileSection
 import com.berlin.aflami.screens.profile.components.SettingSection
+import com.berlin.aflami.screens.profile.components.SettingsDialog
 import com.berlin.aflami.screens.profile.components.WatchHistoryRatingSection
 import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
+import com.berlin.aflami.viewmodel.profile.ProfileDialogType
+import com.berlin.aflami.viewmodel.profile.ProfileInteractionListener
+import com.berlin.aflami.viewmodel.profile.ProfileUiState
+import com.berlin.aflami.viewmodel.profile.ProfileViewModel
 import com.berlin.ui.R
 
 @Composable
-fun ProfileScreen() {
-    ProfileContent()
+fun ProfileScreen(
+    viewModel: ProfileViewModel = hiltViewModel()
+) {
+    val profileScreenState by viewModel.state.collectAsStateWithLifecycle()
+    val navController = Theme.navController
+    ProfileContent(profileScreenState, viewModel)
 }
 
 @Composable
-private fun ProfileContent() {
+private fun ProfileContent(
+    profileScreenState: ProfileUiState,
+    profileScreenInteractionListener: ProfileInteractionListener,
+) {
+
+    when (profileScreenState.activeDialog) {
+        ProfileDialogType.THEME -> {
+            OptionsDialog(
+                onDismiss = { profileScreenInteractionListener.onDialogDismissed() },
+                onApplyClick = {},
+                title = R.string.app_theme,
+                firstOptionTitleRes = com.berlin.ui.R.string.dark,
+                secondOptionTitleRes = com.berlin.ui.R.string.light,
+                firstOptionIconRes = com.berlin.designsystem.R.drawable.dark,
+                secondOptionIconRes = com.berlin.designsystem.R.drawable.light,
+            )
+        }
+
+        ProfileDialogType.LANGUAGE -> {
+            OptionsDialog(
+                onDismiss = { profileScreenInteractionListener.onDialogDismissed() },
+                onApplyClick = { },
+                title = R.string.language,
+                firstOptionTitleRes = R.string.language_dialog_english,
+                secondOptionTitleRes = R.string.language_dialog_arabic,
+                firstOptionIconRes = com.berlin.designsystem.R.drawable.english,
+                secondOptionIconRes = com.berlin.designsystem.R.drawable.arabic,
+            )
+
+        }
+
+        ProfileDialogType.SETTINGS -> {
+            SettingsDialog(
+                onDismiss = { profileScreenInteractionListener.onDialogDismissed() },
+            )
+        }
+
+        else -> Unit
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
@@ -43,11 +95,11 @@ private fun ProfileContent() {
         HorizontalDivider(thickness = 1.dp, color = Theme.color.stroke)
         Spacer(modifier = Modifier.height(24.dp))
         SettingSection(
-            isLanguageEN = true,
-            isDarkThemeEnabled = true,
-            onThemeClick = { /* Handle theme change */ },
-            onLanguageClick = { /* Handle language change */ },
-            onSettingsClick = { /* Handle settings click */ },
+            isLanguageEN = profileScreenState.isLanguageEN,
+            isDarkThemeEnabled = profileScreenState.isDarkThemeEnabled,
+            onThemeClick = { profileScreenInteractionListener.onAppThemeClick() },
+            onLanguageClick = { profileScreenInteractionListener.onLanguageClick() },
+            onSettingsClick = { profileScreenInteractionListener.onSettingsClick() },
         )
 
     }
