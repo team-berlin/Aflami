4defa9c1d0381453e7d29d3cee5d19c79bfa62b7
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-08-11 19:41:24 +0300

    feat: add dislike button to MoviesListItem for user interaction

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/listdetails/component/MoviesListItem.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/listdetails/component/MoviesListItem.kt
@@ -1,23 +1,30 @@
 package com.berlin.aflami.screens.listdetails.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemKey
-import com.berlin.aflami.component.IconButton
 import com.berlin.aflami.component.MediaCard
+import com.berlin.aflami.ui.theme.Theme
+import com.berlin.aflami.viewmodel.shareduistate.MediaType
 import com.berlin.aflami.viewmodel.shareduistate.MovieUiState
 import com.berlin.ui.R
 
@@ -42,22 +49,35 @@ fun MoviesListItem(
             count = movies.itemCount, key = movies.itemKey { it.id }) { index ->
             val movie = movies[index] ?: return@items
             Box {
+                Box(
+                    Modifier
+                        .align(Alignment.TopStart)
+                        .size(32.dp)
+                        .padding(start = 4.dp, top = 4.dp)
+                        .background(
+                            color = Theme.color.iconBackground,
+                        )
+                        .clip(RoundedCornerShape(12.dp))
+                        .clickable {
+                            onClickDislikeItem(listId, movie.id)
+                        },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.heart),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = Theme.color.statusColors.redAccent
+                    )
+                }
                 MediaCard(
+                    modifier = modifier.height(222.dp),
                     mediaImg = movie.posterUrl,
                     title = movie.title,
                     date = movie.releaseDate,
                     rating = movie.rating,
-                    typeOfMedia = stringResource(R.string.movie),
-                    onClick = { onClickMovie(movie.id) })
-
-                IconButton(
-                    modifier = modifier
-                        .padding(start = 4.dp, top = 4.dp)
-                        .size(32.dp),
-                    paddingValues = PaddingValues(6.dp),
-                    painter = painterResource(R.drawable.heart),
-                    contentDescription = null,
-                    onClick = { onClickDislikeItem(listId, movie.id) },
+                    typeOfMedia = MediaType.MOVIE.name,
+                    onClick = { onClickMovie(movie.id) },
                 )
             }
         }
