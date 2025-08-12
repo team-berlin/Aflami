19234a080eb61bb3a3c10fba013d9beb7db95b3b
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-08-12 23:51:59 +0300

    use get points use case in game view model.

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/game/GameViewModel.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/game/GameViewModel.kt
@@ -3,9 +3,12 @@ package com.berlin.aflami.viewmodel.game
 import com.berlin.aflami.viewmodel.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import usecase.game.GetPointsUseCase
 
 @HiltViewModel
-class GameViewModel @Inject constructor() : BaseViewModel<GameScreenState, GameEffect>(GameScreenState()), GameInteractionListener {
+class GameViewModel @Inject constructor(
+    private val getPointsUseCase: GetPointsUseCase
+) : BaseViewModel<GameScreenState, GameEffect>(GameScreenState()), GameInteractionListener {
 
     init {
         loadData()
@@ -17,13 +20,13 @@ class GameViewModel @Inject constructor() : BaseViewModel<GameScreenState, GameE
             GameLevel(10, 100, GameLevelType.MEDIUM, 30),
             GameLevel(10, 100, GameLevelType.HARD, 30)
         )
-
-        updateState {
-            it.copy(
-                points = 100, // TODO: Replace with getPointsUseCase()
-                gameLevel = levels
-            )
-        }
+        tryToCall(
+            call = { getPointsUseCase(1) },// Replace 1 with the user's ID
+            onSuccess = { points ->
+                updateState { it.copy(points = points, gameLevel = levels) }
+            },
+            onError = { updateState { it.copy(points = 0, gameLevel = levels) } }
+        )
     }
 
     override fun onGameInfoClicked(gameType: GameType) =
