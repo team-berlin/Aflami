e5faed530f067581c331c187bd7ec5dc6afb399c
NadeenRR <nadeenradwan0@gmail.com>
2025-08-13 17:29:47 +0300

    feat: implement NoPointDialog component

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/games/components/NoPointDialog.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/games/components/NoPointDialog.kt
@@ -1,16 +1,31 @@
 package com.berlin.aflami.screens.games.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.berlin.aflami.component.buttons.PrimaryButton
 import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
+import com.berlin.ui.R
 
 @Composable
 fun NoPointDialog(
@@ -26,17 +41,61 @@ fun NoPointDialog(
                 )
                 .padding(12.dp)
         ) {
-            // TODO
+            Column(
+                Modifier.widthIn(min = 328.dp, max = 360.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(24.dp)
+            ){
+                Row(
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.not_enough_points),
+                        style = Theme.textStyle.title.large,
+                        color = Theme.color.textColors.title,
+                    )
+                    IconButton(
+                        onClick = onDismiss,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .background(Theme.color.surfaceHigh, shape = RoundedCornerShape(12.dp))
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null,
+                            tint = Theme.color.textColors.title
+                        )
+                    }
+                }
 
+                Text(
+                    text = stringResource(R.string.not_enough_points_for_hint),
+                    style = Theme.textStyle.body.medium,
+                    color = Theme.color.textColors.body,
+                )
+
+                PrimaryButton(
+                    onClick = {},
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(R.string.ok),
+                        style = Theme.textStyle.label.large,
+                        color = Theme.color.textColors.onPrimary
+                    )
+                }
+            }
         }
     }
-
 }
 
 
-@Preview
+@PreviewLightDark
 @Composable
-fun NoPointDialogPreview(modifier: Modifier = Modifier) {
+fun NoPointDialogPreview() {
     AflamiTheme {
         NoPointDialog({})
     }
