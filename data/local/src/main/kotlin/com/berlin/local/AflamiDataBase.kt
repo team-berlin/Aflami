243c0a48ae6d0611c5d30676adfa83ce6a61da41
khaatteerr <hamdikhater68@gmail.com>
2025-08-14 11:21:48 +0300

    fix reload splash screen during other screens configuration

--- data/local/src/main/kotlin/com/berlin/local/AflamiDataBase.kt
+++ data/local/src/main/kotlin/com/berlin/local/AflamiDataBase.kt
@@ -42,7 +42,7 @@ import com.berlin.repository.datasource.local.dto.UserProfileEntity
         TVShowHomeEntity::class,
         UserProfileEntity::class
 
-    ], version =3 , exportSchema = false
+    ], version =4 , exportSchema = false
 )
 abstract class AflamiDatabase : RoomDatabase() {
     abstract fun searchDao(): SearchDao
