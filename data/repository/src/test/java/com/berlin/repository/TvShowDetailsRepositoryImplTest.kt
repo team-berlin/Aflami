328f6c0f92584fd503bceff91fa1cb7b2d7090f8
norhanadel <norhanadel367@gmail.com>
2025-08-16 18:56:56 +0300

    correct unite test TvShowDetailsRepositoryImplTest , MovieDetailsRepositoryImplTest

--- data/repository/src/test/java/com/berlin/repository/TvShowDetailsRepositoryImplTest.kt
+++ data/repository/src/test/java/com/berlin/repository/TvShowDetailsRepositoryImplTest.kt
@@ -21,7 +21,6 @@ import io.mockk.*
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class TvShowDetailsRepositoryImplTest {
 
