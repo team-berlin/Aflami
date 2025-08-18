11608c799f16a22e5e3f6f468830e61fb80a83a7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-08-18 08:46:01 +0300

    fix: enhance LanguageInterceptor to use GetLanguageUseCase for dynamic language handling

--- app/src/main/java/com/berlin/aflami/util/LanguageInterceptor.kt
+++ app/src/main/java/com/berlin/aflami/util/LanguageInterceptor.kt
@@ -1,10 +1,15 @@
 package com.berlin.aflami.util
 
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import usecase.profile.GetLanguageUseCase
 import java.util.Locale
 
-class LanguageInterceptor : Interceptor {
+class LanguageInterceptor(
+    val getLanguageUseCase: GetLanguageUseCase
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val originalUrl = original.url
@@ -14,18 +19,15 @@ class LanguageInterceptor : Interceptor {
 
         val newUrlBuilder = originalUrl.newBuilder()
         if (!shouldSkipLanguage) {
+            val tmdbLanguageParam = runBlocking {
+                val lang = getLanguageUseCase().first().orEmpty().lowercase(Locale.ROOT)
+                if (lang == "ar") "ar-EG" else "${lang}-US"
+            }
             newUrlBuilder.addQueryParameter("language", tmdbLanguageParam)
         }
-
         val newRequest = original.newBuilder()
             .url(newUrlBuilder.build())
             .build()
-
         return chain.proceed(newRequest)
     }
-
-    private val currentLocale = Locale.getDefault()
-    private val languageCode = currentLocale.language
-    private val countryCode = currentLocale.country
-    private val tmdbLanguageParam = "${languageCode}-${countryCode}"
 }
