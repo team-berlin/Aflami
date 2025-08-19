1b5982dcec2534bbf56000f97f948e3541c33f1d
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-08-19 11:55:54 +0300

    refactor: remove unused properties from MainUiState

--- presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/main/MainUiState.kt
+++ presentation/viewModel/src/main/kotlin/com/berlin/aflami/viewmodel/main/MainUiState.kt
@@ -1,12 +1,8 @@
 package com.berlin.aflami.viewmodel.main
 
-import java.util.Locale
-
 data class MainUiState(
     val isLoading: Boolean = true,
     val isLoggedIn: Boolean = false,
     val isFirstEntry: Boolean = false,
     val isDark: Boolean = false,
-    val isDarkThemeEnabled: Boolean = true,
-    val selectedLanguage: String = Locale.getDefault().language.uppercase(),
 )
