adadbead848f8bebdc2f8881cd20296d9209296e
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-08-19 09:22:36 +0300

    refactor:  ProfileViewModel and related effect handling in MainActivity

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mainactivity/MainActivity.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mainactivity/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -18,15 +17,11 @@ import com.berlin.aflami.ui.theme.AflamiTheme
 import com.berlin.aflami.ui.theme.Theme
 import com.berlin.aflami.utils.UpdateLocale
 import com.berlin.aflami.viewmodel.main.MainActivityViewModel
-import com.berlin.aflami.viewmodel.profile.ProfileScreenEffect
-import com.berlin.aflami.viewmodel.profile.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val mainActivityViewModel: MainActivityViewModel by viewModels()
-    private val profileViewModel: ProfileViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
@@ -38,14 +33,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             val mainState by mainActivityViewModel.state.collectAsState()
             val isDarkTheme = mainState.isDarkThemeEnabled
-            LaunchedEffect(Unit) {
-                profileViewModel.effect.collect { effect ->
-                    when (effect) {
-                        is ProfileScreenEffect.RefreshActivity -> recreate()
-                        else -> Unit
-                    }
-                }
-            }
             UpdateLocale(mainState.selectedLanguage)
             AflamiTheme(
                 isDarkTheme = isDarkTheme,
