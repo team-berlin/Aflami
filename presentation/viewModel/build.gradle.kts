f1affaee7040dba0832b984cc69e5a9990ad131e
mohamedshemees <mohamedessampd@gmail.com>
2025-08-19 09:28:11 +0300

    Remove unnecessary blank lines

--- presentation/viewModel/build.gradle.kts
+++ presentation/viewModel/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
 
     // Navigation
     implementation(libs.navigation.compose)
