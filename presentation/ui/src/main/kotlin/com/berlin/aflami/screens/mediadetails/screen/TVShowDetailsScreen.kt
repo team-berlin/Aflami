b7674f2a657b9e0dfd580156026c5d2a04c7ed2e
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-08-20 22:02:25 +0300

    fix: remove unsupported feature dialog handling from TVShow and Movie details screens

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mediadetails/screen/TVShowDetailsScreen.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mediadetails/screen/TVShowDetailsScreen.kt
@@ -96,16 +96,7 @@ fun TvShowDetailsScreen(
             text = stringResource(com.berlin.ui.R.string.loading)
         )
     }
-    AnimatedVisibility(
-        enter =  EnterTransition.None ,
-        exit = ExitTransition.None ,
-        visible = uiState.isNotSupportedFeatureDialogVisible
-    ) {
-        NotSupportedFeatureDialog(
-            description = stringResource(com.berlin.ui.R.string.not_supported_feature),
-            onDismiss = { viewModel.onCancelAddingToFavouriteClicked() }
-        )
-    }
+
     AnimatedVisibility(
         enter =  EnterTransition.None ,
         exit = ExitTransition.None ,
