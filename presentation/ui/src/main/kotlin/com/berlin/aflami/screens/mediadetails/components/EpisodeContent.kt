699584ab819d459dbeb9b4b849137e50ff15fe37
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-08-22 06:09:26 +0300

    refactor: enhance TV show row section display logic and separate it

--- presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mediadetails/components/EpisodeContent.kt
+++ presentation/ui/src/main/kotlin/com/berlin/aflami/screens/mediadetails/components/EpisodeContent.kt
@@ -1,14 +1,20 @@
 package com.berlin.aflami.screens.mediadetails.components
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -17,15 +23,97 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.berlin.aflami.ui.theme.Theme
+import com.berlin.aflami.viewmodel.details.common.MediaDetailsScreenInteractionListener
 import com.berlin.aflami.viewmodel.details.series.EpisodeUiState
+import com.berlin.aflami.viewmodel.details.series.TVShowRowSectionUiState
+import com.berlin.aflami.viewmodel.details.series.TVShowTabContent
 import com.berlin.designsystem.R
 
+fun LazyListScope.tvShowRowSection(
+    state: TVShowRowSectionUiState,
+    listener: MediaDetailsScreenInteractionListener,
+    content: @Composable BoxScope
+    .(
+        state: TVShowRowSectionUiState,
+        listener: MediaDetailsScreenInteractionListener
+    ) -> Unit
+) {
+    item {
+        Crossfade(targetState = state) { tvShowRowSectionUiState ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateContentSize()
+            ) {
+                content(tvShowRowSectionUiState, listener)
+            }
 
+        }
+    }
+}
+
+fun LazyListScope.seasonItem(
+    state: TVShowRowSectionUiState,
+    expandedStates: SnapshotStateMap<Int, Boolean>,
+) {
+    if (state is TVShowRowSectionUiState.Success) {
+        val tab = state.content
+        if (tab is TVShowTabContent.Season) {
+            tab.seasonToEpisodesMap.forEach { (seasonNumber, episodes) ->
+                val isExpanded = expandedStates[seasonNumber] ?: false
+
+                stickyHeader(key = "season_$seasonNumber")
+                {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Theme.color.surface)
+                    )
+                    {
+                        SeasonsHeader(
+                            modifier = Modifier.padding(vertical = 12.dp),
+                            seasonNumber = seasonNumber.toString(),
+                            episodeCount = episodes.size.toString(),
+                            isExpanded = isExpanded,
+                            onToggleExpand = {
+                                expandedStates[seasonNumber] = !isExpanded
+                            }
+                        )
+
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp),
+                            color = Theme.color.stroke,
+                            thickness = 1.dp
+                        )
+                    }
+                }
+                if (isExpanded) {
+                    items(
+                        items = episodes,
+                        key = { episode -> "episode_${seasonNumber}_${episode.id}" }
+                    )
+                    { episode ->
+                        EpisodeCard(
+                            episode = episode,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 12.dp),
+                            onClickPlay = {}
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 @Composable
 fun SeasonsSection(
     seasonsMap: MutableMap<Int, List<EpisodeUiState>>,
