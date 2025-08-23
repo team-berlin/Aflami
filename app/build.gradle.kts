3e4aa0af2b9e213d30cc10bc2209a53d4206819c
mohamedshemees <72915905+mohamedshemees@users.noreply.github.com>
2025-08-23 22:09:23 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,14 +28,6 @@ android {
         }
     }
    
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-            isUniversalApk = true
-        }
-    }
     lint {
         disable += "FlowOperatorInvokedInComposition"
     }
